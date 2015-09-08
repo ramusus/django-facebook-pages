cbe5c862d059d3989068cbe27912d19b2852dabf
ramusus <ramusus@gmail.com>
2015-09-08 14:29:59 +0300

    udated admin

--- facebook_pages/admin.py
+++ facebook_pages/admin.py
@@ -3,7 +3,9 @@ from django.contrib import admin
 from facebook_api.admin import FacebookModelAdmin
 from models import Page
 
+
 class PageAdmin(FacebookModelAdmin):
+    search_fields = ('name', 'username')
     list_display = ('name','username','category','likes_count','checkins_count','talking_about_count')
     list_filter = ('is_published',)
 
